49085170ba2aa48f9ed2b0fc68d8caf52b178483
berthamz <berthamz@gmail.com>
2022-02-28 13:24:34 -0600

    v1 redo of timeout

--- src/progress_bar_exercise/components/ProgressBarSolution.js
+++ src/progress_bar_exercise/components/ProgressBarSolution.js
@@ -1,37 +1,47 @@
-import React from 'react'
-import './ProgressBarSolution.scss'
+import React from "react";
+import "./ProgressBarSolution.scss";
 
 const ProgressBarSolution = () => {
-    const [status, setStatus] = React.useState('idle')
-    const [isLoading, setIsLoading] = React.useState(false)
-    
-    const changeTitle = () => {
-        setStatus('active') 
-        setIsLoading(true)
-    };
-    
-    const endRequest = () => {
-        setStatus('ended')
-        setIsLoading(false)
+  const [status, setStatus] = React.useState("idle");
+  const [isLoading, setIsLoading] = React.useState(false);
 
-        setTimeout(() => {
-            setStatus('idle')
-        }, 5000)
+  React.useEffect(() => {
+    if (status === "ended") {
+      const idleTimeout = setTimeout(() => {
+        setStatus("idle");
+      }, 5000);
+
+      return () => {
+        clearTimeout(idleTimeout);
+      };
     }
+  }, [status]);
 
-    return (
-        <div className={status}>
-            <div className='progress-bar-container'>
-                <div className='progress-bar'></div>
-            </div>
-            <div className='buttons-container'>
-                <button className='button start-request' onClick={changeTitle}>
-                    {isLoading ? "Loading..." : "Start Request"}
-                </button>
-                <button className='button finish-request' onClick={endRequest}>Finish Request</button>
-            </div>
-        </div>
-    )
-}
+  const startRequest = () => {
+    setStatus("active");
+    setIsLoading(true);
+  };
+
+  const endRequest = () => {
+    setStatus("ended");
+    setIsLoading(false);
+  };
+
+  return (
+    <div className={status}>
+      <div className="progress-bar-container">
+        <div className="progress-bar"></div>
+      </div>
+      <div className="buttons-container">
+        <button className="button start-request" onClick={startRequest}>
+          {isLoading ? "Loading..." : "Start Request"}
+        </button>
+        <button className="button finish-request" onClick={endRequest}>
+          Finish Request
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ProgressBarSolution;
